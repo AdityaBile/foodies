951f8dc342fc3e2b241ac2bead00cd7a450b5c8a
Aditya Bile <adityabile30602@gmail.com>
2024-04-17 16:51:13 +0530

    Adding header

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,12 @@
+// localhost:3000/meals/:mealId(something)
+import React from "react";
+
+const MealDetailPage = () => {
+  return (
+    <div>
+      <h1>Meal Detail Page</h1>
+    </div>
+  );
+};
+
+export default MealDetailPage;
